c8512e091653cacd8b7295fc1bca783258d42182
Julius2333 <920038773@qq.com>
2020-08-29 11:09:32 +0800

    增加一个python蟒蛇绘制temp

---
+++ study/python/1.3温度转换/transport.py
@@ -0,0 +1,23 @@
+tmp = input()
+if eval(tmp) == 0 :
+    print("Hello World")
+elif eval(tmp) < 0 :
+    print("H\n")
+    print("e\n")
+    print("l\n")
+    print("l\n")
+    print("o\n")
+    print(" \n")
+    print("W\n")
+    print("o\n")
+    print("r\n")
+    print("l\n")
+    print("d\n")
+else :
+    print("He\n")
+    print("ll\n")
+    print("o \n")
+    print("Wo\n")
+    print("rl\n")
+    print("d\n")
+
